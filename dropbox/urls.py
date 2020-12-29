c4d08897b4d5e0b97a3c0064c817d22e08f6dec0
sahilss1499 <sahilss1499@gmail.com>
2020-12-29 18:12:11 +0530

    created upload  model

--- dropbox/urls.py
+++ dropbox/urls.py
@@ -19,5 +19,5 @@ from users import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
 ]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Upload(models.Model):
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    file_upload = models.FileField(upload_to='users')
